2e637fe1818441f2c991a5bdddfa7cf77eb7b674
Ioannis Oikonomidis <Isoi@hempel.com>
2016-02-09 16:59:25 +0100

    applying feedback on getting and cleaning data final project

--- Getting and Cleaning Data/Project/run_analysis.R
+++ Getting and Cleaning Data/Project/run_analysis.R
@@ -8,68 +8,61 @@
 # 5) From the data set in step 4, creates a second, independent tidy data set with the average of
 # each variable for each activity and each subject.
 
-# Importing needed libraries
+# Installing and importing needed libraries
+install.packages('dplyr')
+install.packages('stringr')
+install.packages('reshape2')
 library(dplyr)
 library(stringr)
 library(reshape2)
 
 # Reading files
-test_labels <- read.table("./data/test/y_test.txt", col.names = c("label"))
-train_labels <- read.table("./data/train/y_train.txt", col.names = c("label"))
-activities <- read.table("./data/activity_labels.txt", col.names = c("label", "name"))
-train_subject <- read.table("./data/train/subject_train.txt", col.names = c("subject"))
-test_subject <- read.table("./data/test/subject_test.txt", col.names = c("subject"))
-test <- read.table("./data/test/X_test.txt")
-train <- read.table("./data/train/X_train.txt")
-features <- read.table("./data/features.txt")
+activityTest <- read.table("./data/test/y_test.txt", col.names = c("activityLabel"))
+activityTrain <- read.table("./data/train/y_train.txt", col.names = c("activityLabel"))
+activityLabels <- read.table("./data/activity_labels.txt", col.names = c("activityLabel", "activityName"))
+subjectTrain <- read.table("./data/train/subject_train.txt", col.names = c("subject"))
+subjectTest <- read.table("./data/test/subject_test.txt", col.names = c("subject"))
+featuresTest <- read.table("./data/test/X_test.txt")
+featuresTrain <- read.table("./data/train/X_train.txt")
+featureNames <- read.table("./data/features.txt")
 
-# Introducing activity names into labels
-test_labels <- test_labels %>%
-                merge(activities, by.x = "label", by.y = "label")
-train_labels <- train_labels %>%
-        merge(activities, by.x = "label", by.y = "label")
 
 # 1) Merges the training and the test sets to create one data set.
-merged_data <- rbind(train, test)
-merged_labels <- rbind(train_labels, test_labels)
-merged_subjects <- rbind(train_subject, test_subject)
-
+dataSubject <- rbind(subjectTrain, subjectTest)
+dataActivity<- rbind(activityTrain, activityTest)
+dataFeatures<- rbind(featuresTrain, featuresTest)
 
 
 # 2) Extracts only the measurements on the mean and standard deviation for each measurement.
-
 # Regex escape parenthesis()
-features_of_interest <- grep("mean\\(\\)|std\\(\\)", features$V2)
-filtered_features <- features[features_of_interest, ]
-# Extracted data
-merged_data <- merged_data[, features_of_interest]
-names(merged_data) <- filtered_features$V2 %>% str_replace_all("\\(\\)", "")
-
+featuresOfInterest <- grep("mean\\(\\)|std\\(\\)", featureNames$V2)
+filteredFeatures <- featureNames[featuresOfInterest, ]
+filteredDataFeatures <- dataFeatures[, filteredFeatures$V2]
+names(filteredDataFeatures)<- filteredFeatures$V2 %>% str_replace_all("\\(\\)", "")
 
 
 # 3) Uses descriptive activity names to name the activities in the data set
-merged_data_with_activities <- merged_data %>% mutate(activity = merged_labels$label)
-
-
-
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(filteredDataFeatures, dataCombine)
+Data$activity <- factor(Data$activity, levels = activityLabels[,1], labels = activityLabels[,2])
 
 
 # 4) Appropriately labels the data set with descriptive variable names
-# Rename header
-names(merged_data_with_activities) <- names(merged_data_with_activities) %>% str_replace_all("\\(\\)", "") %>%
-        str_replace_all("mean", "Mean") %>%
-        str_replace_all("std", "Std") %>%
-        str_replace_all("-", "")
-
-
+names(Data) <- names(Data) %>% str_replace_all("\\(\\)", "") %>%
+  str_replace_all("mean", "Mean") %>%
+  str_replace_all("std", "Std") %>%
+  str_replace_all("-", "") %>%
+  str_replace_all("^t", "time") %>%
+  str_replace_all("^f", "frequency") %>%
+  str_replace_all("Acc", "Accelerometer") %>%
+  str_replace_all("Gyro", "Gyroscope") %>%
+  str_replace_all("Mag", "Magnitude") %>%
+  str_replace_all("BodyBody", "Body")
 
 
 # 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-merged_data_with_activities_and_subjects <- merged_data_with_activities %>% mutate(subject = merged_subjects$subject)
-final.melted <- melt(merged_data_with_activities_and_subjects, id = c("subject", "activity"))
+final.melted <- melt(Data, id = c("subject", "activity"))
 final.mean <- dcast(final.melted, subject + activity ~ variable, mean) 
-View(final.mean)
-final.mean.ordered <- final.mean %>% arrange(activity, subject)
+final.mean.ordered <- final.mean %>% arrange(subject, activity)
 View(final.mean.ordered)
 write.table(final.mean.ordered, "tidy.txt", row.names = FALSE, quote = FALSE)
-
